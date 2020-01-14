603ec56dfd1bb45dd319bc489e411ab0cc50392c
chiwawa313 <no_pain.no_gain.0313@docomo.ne.jp>
2020-01-14 18:18:49 +0900

    非同期通信

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ Bundler.require(*Rails.groups)
 module ChatSpace
   class Application < Rails::Application
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
